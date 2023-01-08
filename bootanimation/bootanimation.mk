Maxx12211/vendor_lineage

config: Import google boot animation from pe
* from https://github.com/PixelExperience/vendor_aosp/commit/fa63d67221aa6d1334ada08c987c4677cec9ff51

Change-Id: I5f47059944b37362821230b461012cfa04872975
Co-Authored-By: jhenrique09 <jhsv09@gmail.com>

bootanimation/bootanimation.mk
@@ -1,0 +1,13 @@
+# Bootanimation
+ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+     PRODUCT_COPY_FILES += vendor/lineage/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
+     PRODUCT_COPY_FILES += vendor/lineage/bootanimation/bootanimation_1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else
+    ifeq ($(TARGET_BOOT_ANIMATION_RES),)
+        $(warning "TARGET_BOOT_ANIMATION_RES is undefined, assuming 1080p")
+    else
+        $(warning "Current bootanimation res is not supported, forcing 1080p")
+    endif
+    PRODUCT_COPY_FILES += vendor/lineage/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+endif

EpicROM-AOSP/android_vendor_epic

bootanimation/bootanimation.mk
@@ -1,0 +1,13 @@
+# Bootanimation
+ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+     PRODUCT_COPY_FILES += vendor/epic/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
+     PRODUCT_COPY_FILES += vendor/epic/bootanimation/bootanimation_1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else
+    ifeq ($(TARGET_BOOT_ANIMATION_RES),)
+        $(warning "TARGET_BOOT_ANIMATION_RES is undefined, assuming 1080p")
+    else
+        $(warning "Current bootanimation res is not supported, forcing 1080p")
+    endif
+    PRODUCT_COPY_FILES += vendor/epic/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+endif